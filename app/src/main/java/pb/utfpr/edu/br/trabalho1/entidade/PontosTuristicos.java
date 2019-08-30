a638662fba8c3dd68b761b31819293e79c50caef
gmaronezi <gmaronezi@gmail.com>
2019-08-29 22:27:42 -0300

    Criado layout

---
+++ app/src/main/java/pb/utfpr/edu/br/trabalho1/entidade/PontosTuristicos.java
@@ -0,0 +1,58 @@
+package pb.utfpr.edu.br.trabalho1.entidade;
+
+public class PontosTuristicos {
+    private int _id;
+    private String titulo;
+    private String descricao;
+    private String endereco;
+    private Double latitude;
+    private Double longitude;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
