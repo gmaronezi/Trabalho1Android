c48ad4d2df1512262f16f39fa29c58790daa5d2a
gmaronezi <gmaronezi@gmail.com>
2019-08-29 21:55:57 -0300

    Initial commit

---
+++ app/src/main/java/pb/utfpr/edu/br/trabalho1/MainActivity.java
@@ -0,0 +1,13 @@
+package pb.utfpr.edu.br.trabalho1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
