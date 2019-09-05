c09517bd2b2ba11735c69f953eb44cac5e7ffd1e
gmaronezi <gmaronezi@gmail.com>
2019-09-05 02:29:38 -0300

    Removido algumas variáveis que não estavam sendo usadas.

--- app/src/main/java/pb/utfpr/edu/br/trabalho1/AlterarActivity.java
+++ app/src/main/java/pb/utfpr/edu/br/trabalho1/AlterarActivity.java
@@ -41,7 +41,6 @@ public class AlterarActivity extends AppCompatActivity implements LocationListen
     private DatabaseHandler dao;
 
     private PontosTuristicos pontoT;
-    Cursor registros;
     Long id_ponto;
 
     @Override
@@ -168,15 +167,9 @@ public class AlterarActivity extends AppCompatActivity implements LocationListen
 
         try {
 
-
             addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
 
             String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-            String city = addresses.get(0).getLocality();
-            String state = addresses.get(0).getAdminArea();
-            String country = addresses.get(0).getCountryName();
-            String postalCode = addresses.get(0).getPostalCode();
-            String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
 
             tvLatitudeAlterar.setText( String.valueOf( latitude ) );
             tvLongitudeAlterar.setText( String.valueOf( longitude ));
